195b48a141f824cc246bc0a5086bc31035ce0197
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-05-24 12:38:57 -0400

    hi

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -12,17 +12,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
-const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const express_session_1 = __importDefault(require("express-session"));
-const morgan_1 = __importDefault(require("morgan"));
 const connect_mongo_1 = __importDefault(require("connect-mongo"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const environments_1 = __importDefault(require("./environments"));
+const app = (0, express_1.default)();
 dotenv_1.default.config();
 // Import routes and other utilities
 const payments_1 = __importDefault(require("./handlers/payments"));
@@ -30,32 +27,22 @@ const user_1 = __importDefault(require("./handlers/user"));
 const community_1 = __importDefault(require("./handlers/community"));
 const posts_1 = __importDefault(require("./handlers/posts"));
 const comments_1 = __importDefault(require("./handlers/comments"));
+// MongoDB connection
 const mongoURI = process.env.MONGO_URI;
 if (!mongoURI) {
     console.error('MongoDB URI not defined in environment variables');
-    process.exit(1); // Exit if no MongoDB URI is defined
+    process.exit(1);
 }
 // MongoDB connection
 mongoose_1.default.connect(mongoURI)
     .then(() => {
     console.log('Connected to MongoDB URI:', mongoURI);
-    const app = (0, express_1.default)();
-    // Serve static files from the React app
-    app.use(express_1.default.static(path_1.default.join(__dirname, '..', 'frontend/build')));
-    // Log requests to the console in a compact format:
-    app.use((0, morgan_1.default)('dev'));
-    // Full log of all requests to /log/access.log:
-    app.use((0, morgan_1.default)('common', {
-        stream: fs_1.default.createWriteStream(path_1.default.join(__dirname, '..', 'log', 'access.log'), { flags: 'a' }),
-    }));
-    // Enable response bodies to be sent as JSON:
-    app.use(express_1.default.json());
-    // Handle CORS:
+    // Middleware setup
     app.use((0, cors_1.default)({
         origin: "https://www.destigfemme.app",
         credentials: true
     }));
-    app.use((0, cookie_parser_1.default)());
+    app.use(express_1.default.json());
     app.use((0, express_session_1.default)({
         secret: process.env.SESSION_SECRET || 'your_default_secret_value',
         resave: false,
@@ -79,37 +66,40 @@ mongoose_1.default.connect(mongoURI)
     app.locals.postCollection = db.collection('posts');
     app.locals.commentCollection = db.collection('comments');
     console.log('Collections initialized');
+    // Serve static files from the React app
+    app.use(express_1.default.static(path_1.default.join(__dirname, 'frontend/build')));
+    // Routes
+    const userRouter = express_1.default.Router();
+    app.get('/api', (_, res) => __awaiter(void 0, void 0, void 0, function* () {
+        res.status(200).send({ message: "Hello, World!" });
+    }));
     // Endpoint mounting
     const paymentsRouter = express_1.default.Router();
     (0, payments_1.default)(paymentsRouter);
-    app.use('/payments', paymentsRouter);
-    const userRouter = express_1.default.Router();
+    app.use('/api/payments', paymentsRouter);
     (0, user_1.default)(userRouter);
-    app.use('/user', userRouter);
+    app.use('/api/user', userRouter);
     const communityRouter = express_1.default.Router();
     (0, community_1.default)(communityRouter);
-    app.use('/community', communityRouter);
+    app.use('/api/community', communityRouter);
     const postRouter = express_1.default.Router();
     (0, posts_1.default)(postRouter);
-    app.use('/posts', postRouter);
+    app.use('/api/posts', postRouter);
     const commentRouter = express_1.default.Router();
     (0, comments_1.default)(commentRouter);
-    app.use('/comments', commentRouter);
-    app.get('/', (_, res) => __awaiter(void 0, void 0, void 0, function* () {
-        res.status(200).send({ message: "Hello, World!" });
-    }));
+    app.use('/api/comments', commentRouter);
     // The "catchall" handler: for any request that doesn't
     // match one above, send back React's index.html file.
+    // Handle all other routes with React app
     app.get('*', (req, res) => {
-        res.sendFile(path_1.default.join(__dirname, '..', 'frontend/build', 'index.html'));
+        res.sendFile(path_1.default.join(__dirname, 'frontend/build', 'index.html'));
     });
     const port = process.env.PORT || 3001;
     app.listen(port, () => {
-        console.log(`App platform demo app - Backend listening on port ${port}!`);
-        console.log(`CORS configured for frontend hosted on ${environments_1.default.frontend_url}`);
+        console.log(`Backend listening on port ${port}`);
     });
 })
     .catch((err) => {
     console.error('MongoDB connection error:', err);
-    process.exit(1); // Exit if cannot connect to MongoDB
+    process.exit(1);
 });
