95edddb3555352019746f46dbf1c94a2dc971167
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-06-01 19:14:18 -0400

    hi

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './defaults.css';
-import Shop from './Shop/pages/home';
 import App from './Shop/App';
 import { BrowserRouter as Router } from "react-router-dom";
-import axios from 'axios';
-
 
 ReactDOM.render(
   <Router>
@@ -16,4 +13,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
-
