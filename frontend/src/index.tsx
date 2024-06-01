974a9ffaa80e55b6e8a2490c7d1afa89b97c8bf8
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-06-01 17:48:24 -0400

    hi

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,24 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './defaults.css';
-import Shop from './Shop/pages/home';
 import App from './Shop/App';
-import { BrowserRouter as Router } from "react-router-dom";
-import axios from 'axios';
-
-const backendURL = process.env.REACT_APP_BACKEND_URL || 'https://api.destigfemme.com';
-
-const axiosClient = axios.create({ baseURL: backendURL, timeout: 20000, withCredentials: true });
-const config = {headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}};
 
 ReactDOM.render(
-  <Router>
     <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+    <App />
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
-
-export default axiosClient;
